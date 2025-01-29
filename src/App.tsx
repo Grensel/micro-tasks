848daac5ae7e60a0aa90d5fad5d6d6e12136aa76
Grensel <sergeiodinochenko@mail.ru>
2025-01-29 15:20:45 +0300

    init: statrt Associative Array task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import "./App.css";
 import { MicroTask1 } from "./components/microTask1/MicroTask1";
 import { MicroTask2 } from "./components/microTask2/MicroTask2";
+import AssociativeArray from "./components/src/AssociativeArray";
 
 function App() {
   return (
     <>
-      <MicroTask1 />
-      <MicroTask2 />
+      <h1>Micro Tasks, Home Work</h1>
+      <div>
+        <h2>Sprint 1</h2>
+        <MicroTask1 />
+        <MicroTask2 />
+      </div>
+      <div>
+        <h2>Sprint 2</h2>
+        <AssociativeArray />
+      </div>
     </>
   );
 }
